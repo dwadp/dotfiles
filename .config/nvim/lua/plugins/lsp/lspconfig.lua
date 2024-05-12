80a2c8876c182138702b4c2589e80ca90927613d
Adi Perdana <dewaadiperdana@gmail.com>
2024-05-12 19:07:10 +0800

    add specific severity diagnostics keymaps

--- .config/nvim/lua/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/plugins/lsp/lspconfig.lua
@@ -12,8 +12,9 @@ return {
       },
     },
     {
-      "folke/neodev.nvim", opts = {}
-    }
+      "folke/neodev.nvim",
+      opts = {},
+    },
   },
   config = function()
     local lspconfig = require("lspconfig")
@@ -55,18 +56,32 @@ return {
         opts.desc = "Show line diagnostics"
         keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts) -- show diagnostics for line
 
-        opts.desc = "Go to previous diagnostic"
-        keymap.set("n", "[d", vim.diagnostic.goto_prev, opts) -- jump to previous diagnostic in buffer
+        opts.desc = "Go to previous error"
+        keymap.set("n", "[e", function()
+          vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.ERROR })
+        end, opts)
 
-        opts.desc = "Go to next diagnostic"
-        keymap.set("n", "]d", vim.diagnostic.goto_next, opts) -- jump to next diagnostic in buffer
+        opts.desc = "Go to next error"
+        keymap.set("n", "]e", function()
+          vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.ERROR })
+        end, opts)
+
+        opts.desc = "Go to previous warning"
+        keymap.set("n", "[w", function()
+          vim.diagnostic.goto_prev({ severity = vim.diagnostic.severity.WARN })
+        end, opts)
+
+        opts.desc = "Go to next warning"
+        keymap.set("n", "]w", function()
+          vim.diagnostic.goto_next({ severity = vim.diagnostic.severity.WARN })
+        end, opts)
 
         opts.desc = "Show documentation for what is under cursor"
         keymap.set("n", "K", vim.lsp.buf.hover, opts) -- show documentation for what is under cursor
 
         opts.desc = "Restart LSP"
         keymap.set("n", "<leader>rs", ":LspRestart<CR>", opts) -- mapping to restart lsp if necessary
-      end
+      end,
     })
 
     -- used to enable autocompletion (assign to every lsp server config)
@@ -122,5 +137,5 @@ return {
         })
       end,
     })
-  end
+  end,
 }
