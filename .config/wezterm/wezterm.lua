2f716ba0daa5347052dd5c844df65d581da2c468
Adi Perdana <dewaadiperdana@gmail.com>
2024-10-20 13:31:01 +0800

    change font

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,15 +2,16 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.color_scheme = "Night Owl (Gogh)"
-config.font = wezterm.font("CaskaydiaCove Nerd Font", { italic = false })
+config.font = wezterm.font("Maple Mono NF", { italic = true })
+-- config.font = wezterm.font("CaskaydiaCove Nerd Font", { italic = false })
 config.font_size = 12.4
 config.line_height = 1.3
 config.enable_tab_bar = false
 config.window_padding = {
-	left = 0,
-	right = 0,
-	top = 0,
-	bottom = 0,
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 return config
